a60ea8aa01bf59c02b8562ea8e16daf86c8a0115
Brody Davison (RIT Student) <brodydavison4@gmail.com>
2022-02-10 14:50:33 -0500

    Basic Character movement implemented
    Player 1 prefab asset created. Implemented movement controls w/ double jump

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float jumpLimit;
+    [SerializeField] float jumpVelocityClamp;
+
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
+    //Input Handling
+    bool holdingD;
+    bool holdingA;
+    bool holdingJump;
+    bool releaseJump;
+    bool grounded;
+    bool hasDoubleJump;
+
+    float jumpTimer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Input Handling
+        if (Input.GetKey("d"))
+        {
+            holdingD = true;
+        }
+        else
+        {
+            holdingD = false;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            holdingA = true;
+        }
+        else
+        {
+            holdingA = false;
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            holdingJump = true;
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            holdingJump = false;
+        }
+
+        #endregion
+    }
+
+    void FixedUpdate()
+    {
+        if (holdingD)
+        {
+            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+            sr.flipX = false;
+        }
+
+        if (holdingA)
+        {
+            transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0, 0);
+            sr.flipX = true;
+        }
+
+        if (holdingJump && (grounded || hasDoubleJump))
+        {
+            rb.velocity = new Vector2(0, jumpSpeed * Time.deltaTime);
+            jumpTimer += Time.deltaTime;
+        }
+
+        if (jumpTimer >= jumpLimit || (holdingJump == false && jumpTimer != 0))
+        {
+            rb.velocity = new Vector2(0, jumpVelocityClamp);
+            jumpTimer = 0f;
+            holdingJump = false;
+            if (!grounded)
+            {
+                hasDoubleJump = false;
+            }
+            grounded = false;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Platform") && col.GetContact(0).normal.y > 0)
+        {
+            grounded = true;
+            hasDoubleJump = true;
+        }
+    }
+
+}
