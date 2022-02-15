fc798edd2c9c1c115efb1810c56d7e625a32ac8c
JT5519 <joel.sk10@gmail.com>
2022-02-15 15:13:17 -0500

    game manager setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Save Objects")]
+    public GameObject player1;
+
+    private void Start()
+    {
+        
+    }
+    public void OnSave()
+    {
+       //saving player1 stuff
+       
+    }
+}
