c663c401c925ec114c963950e1a08ba1a7d38c9b
JBDdev <brodydavison4@gmail.com>
2022-02-16 21:58:15 -0500

    Game UI Finished
    UI player character icon will deep fry for the duration of powerups. Game UI prefab created

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,17 +7,29 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] GameObject count;
     [SerializeField] GameObject icon;
+    public float colorCycleSpeed = 0.5f;
 
+    Color defaultColor;
+    
     GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+        defaultColor = Color.white;
     }
 
     // Update is called once per frame
     void Update()
     {
-        count.GetComponent<Text>().text = player.GetComponent<PlayerCharacter>().GetLives() + " x"; 
+        count.GetComponent<Text>().text = player.GetComponent<PlayerCharacter>().GetLives() + " x";
+        if (player.GetComponent<PlayerCharacter>().HasPowerUp())
+        {
+            icon.GetComponent<Image>().material.color += new Color(colorCycleSpeed, colorCycleSpeed, colorCycleSpeed, 0);
+        }
+        else 
+        {
+            icon.GetComponent<Image>().material.color = defaultColor;
+        }
     }
 }
